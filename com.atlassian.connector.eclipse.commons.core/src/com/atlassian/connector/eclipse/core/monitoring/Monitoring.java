5134315120c21067290c20547fb61d47d001b509
pure-systems <contact@pure-systems.com>
2020-10-21 12:18:07 +0200

    Removed monitoring.

--- com.atlassian.connector.eclipse.commons.core/src/com/atlassian/connector/eclipse/core/monitoring/Monitoring.java
+++ com.atlassian.connector.eclipse.commons.core/src/com/atlassian/connector/eclipse/core/monitoring/Monitoring.java
@@ -11,49 +11,14 @@
 
 package com.atlassian.connector.eclipse.core.monitoring;
 
-import com.atlassian.connector.eclipse.monitor.core.InteractionEvent;
-import com.atlassian.connector.eclipse.monitor.core.MonitorCorePlugin;
-import com.atlassian.connector.eclipse.monitor.core.InteractionEvent.Kind;
-
-import java.util.Date;
-
 public final class Monitoring {
-	private final String pluginId;
-
-	private boolean available;
 
 	public Monitoring(String pluginId) {
-		this.pluginId = pluginId;
-
-		try {
-			Class.forName("com.atlassian.connector.eclipse.monitor.core.MonitorCorePlugin");
-			available = true;
-		} catch (ClassNotFoundException e) {
-			available = false;
-		}
 	}
 
 	public void logAction(String actionId, String details) {
-		if (!available) {
-			return;
-		}
-
-		MonitorCorePlugin plugin = MonitorCorePlugin.getDefault();
-		if (plugin != null && plugin.getInteractionLogger() != null) {
-			plugin.getInteractionLogger().interactionObserved(
-					new InteractionEvent(Kind.ACTION, pluginId, actionId, details, new Date()));
-		}
 	}
 
 	public void logJob(String jobId, String details) {
-		if (!available) {
-			return;
-		}
-
-		MonitorCorePlugin plugin = MonitorCorePlugin.getDefault();
-		if (plugin != null && plugin.getInteractionLogger() != null) {
-			plugin.getInteractionLogger().interactionObserved(
-					new InteractionEvent(Kind.JOB, pluginId, jobId, details, new Date()));
-		}
 	}
 }
